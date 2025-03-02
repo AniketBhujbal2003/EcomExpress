36d010a040498153fcbfda82ec5fb498ddf65d8c
Aniket <aniket@gmail.com>
2025-03-02 13:21:29 +0530

    change cookie congig

--- server/controlers/auth_controllers.js
+++ server/controlers/auth_controllers.js
@@ -102,7 +102,7 @@ const loginUser = async (req, res) => {
             }
         )
 
-        res.cookie('token',token,{httpOnly:true,secure:false}).json(
+        res.cookie('token',token,{httpOnly:true,secure:true}).json(
             {
                 success:true,
                 message: 'LoggedIn Successfull',
